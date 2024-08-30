fdc1040ea66d92f59b60735ecc6cc60d2ab5e9cf
Alberto Gucciardi <studio@agconsulente.com>
2024-08-30 13:05:28 +0200

    Set of parameters and correctness check - review request and comments  on next commit

--- src/boilsys.ps1
+++ src/boilsys.ps1
@@ -10,3 +10,43 @@
  # @dependencies: None
  #>
 
+# Setup all parameters from console
+Param(
+    $ProjectName='',
+    $Path=$HOME,
+    $GitUserName='',
+    $GitBaseRepo='master',
+    $ProjectLanguage='PHP'
+)
+function Get-ParameterCheck {
+    if($ProjectName -eq '') {
+        $ProjectName = Read-Host "Enter the project name";
+    }
+    if($Path -eq $HOME) {
+        $Respone = Read-Host "Do you want to create the project in the current directory? (Y/N)";
+        if($Respone -eq 'N') {
+            $Path = Read-Host "Enter the path where you want to create the project";
+        }
+    }
+    if($GitUserName -eq '') {
+        $GitUserName = Read-Host "Enter your git username";
+    }
+    if($GitBaseRepo -eq 'master') {
+        $Respone = Read-Host "Do you want to use a different base repository? (Y/N)";
+        if($Respone -eq 'Y') {
+            $GitBaseRepo = Read-Host "Enter the base repository";
+        }
+    }
+    if( $ProjectLanguage -eq 'PHP') {
+        $Respone = Read-Host "Do you want to use a different language? (Y/N)";
+        if($Respone -eq 'Y') {
+            $ProjectLanguage = Read-Host "Enter the project language";
+        }
+    }
+}
+
+# Start and test point
+function Main{
+    Get-ParameterCheck
+}
+Main
