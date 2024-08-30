bbc8a02b1234c3bc88da8b5d24963af2cee6435f
Alberto Gucciardi <studio@agconsulente.com>
2024-08-30 12:57:54 +0200

    Get info of project

--- src/boilsys.ps1
+++ src/boilsys.ps1
@@ -1,2 +1,12 @@
-# Making a new way to boilerplating projects
+<#
+ # @project: boilsys
+ # @created: 2024-08-29
+ # @description: Boilerplate system for projects
+ # @version: 0.1.0
+ # @license: MIT
+ # @author: Alberto Gucciardi Consulente
+ # @website: https://www.agconsulente.net/boilsys
+ # @repository: https://github.com/agconsulente/boilsys
+ # @dependencies: None
+ #>
 
